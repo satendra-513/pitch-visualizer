4b791047d4ec86ebae956957918d0d974eb89f05
satendra-513 <satendramrj3@gmail.com>
2026-04-07 00:38:14 +0530

    fix requirements

--- utils/segment.py
+++ utils/segment.py
@@ -3,7 +3,5 @@ nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
 
 def segment_text(text):
-    sentences = sent_tokenize(text)
-    
-    # minimum 3 segments
-    return sentences[:5] if len(sentences) >= 3 else sentences
+    sentences = text.split('. ')
+    return [s.strip() for s in sentences if s]
